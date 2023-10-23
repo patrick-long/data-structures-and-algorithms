a72dc2f712ed55b91458112bdd817b4eea604d6a
Patrick Long <patrick@deepspacerobots.com>
2023-10-23 08:58:23 -0500

    implement Queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,57 @@
+/*
+	A Queue is simply a FIFO implementation of a Linked List
+
+	Time Complexity:
+		- enqueue: O(1)
+		- dequeue: O(1)
+		- peek: O(n)
+*/
+
+type Node<T> = {
+  value: T;
+  next?: Node<T>;
+};
+
 export default class Queue<T> {
-    public length: number;
+  public length: number;
+  private head?: Node<T>;
+  private tail?: Node<T>;
 
-    
+  constructor() {
+    this.head = this.tail = undefined;
+    this.length = 0;
+  }
 
-    constructor() {
+  enqueue(item: T): void {
+    const node = { value: item } as Node<T>;
+    this.length++;
+
+    if (!this.tail) {
+      this.tail = this.head = node;
+      return;
     }
 
-    enqueue(item: T): void {
+    this.tail.next = node;
+    this.tail = node;
+  }
 
-}
-    deque(): T | undefined {
+  deque(): T | undefined {
+    if (!this.head) {
+      return undefined;
+    }
 
-}
-    peek(): T | undefined {
+    this.length--;
 
-}
+    const tmp = this.head;
+    this.head = this.head.next;
+
+    // free
+    tmp.next = undefined;
+
+    return tmp.value;
+  }
+
+  peek(): T | undefined {
+    return this.head?.value;
+  }
 }
