25281053c5955a2ea2b54bfe296473b39183dbb9
Patrick Long <patrick@deepspacerobots.com>
2023-10-23 07:43:31 -0500

    write Binary Search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,35 @@
-export default function bs_list(haystack: number[], needle: number): boolean {
+/*
+	search(arr, lo, hi, needle)
 
+	midpoint = lo + (hi - lo) / 2
+	value = arr[midpoint]
+
+	if value is equal to needle, then return true
+
+	[ = inclusive, ) = exclusive
+	[lo, hi)
+	if value is larger than midpoint, then lo should now equal midpoint + 1
+	if value is smaller than midpoint, then hi should now equal midpoint
+
+	while lo is less than hi, recursively call function with new parameters
+*/
+
+export default function bs_list(haystack: number[], needle: number): boolean {
+  let lo = 0;
+  let hi = haystack.length;
+
+  do {
+    const midpoint = Math.floor(lo + (hi - lo) / 2);
+    const value = haystack[midpoint];
+
+    if (value === needle) {
+      return true;
+    } else if (value > needle) {
+      hi = midpoint;
+    } else {
+      lo = midpoint + 1;
+    }
+  } while (lo < hi);
+
+  return false;
 }
