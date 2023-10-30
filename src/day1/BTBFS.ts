187421bb7aa39e75d10fc3ba5a37459df22f090c
Patrick Long <patrick@deepspacerobots.com>
2023-10-30 08:51:28 -0500

    write Binary Tree Breadth First Search

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,25 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+  const queue: (BinaryNode<number> | null)[] = [head];
 
+  while (queue.length) {
+    const curr = queue.shift() as BinaryNode<number>;
+
+    if (!curr) {
+      continue;
+    }
+
+    if (curr.value === needle) {
+      return true;
+    }
+
+    if (curr.left) {
+      queue.push(curr.left);
+    }
+
+    if (curr.right) {
+      queue.push(curr.right);
+    }
+  }
+
+  return false;
 }
