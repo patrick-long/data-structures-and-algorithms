17967165675a98d7ba96ec68892136a7ea75d1ec
Patrick Long <patrick@deepspacerobots.com>
2023-11-06 07:26:12 -0600

    write Depth First Search on Graph List

--- src/day1/DFSGraphList.ts
+++ src/day1/DFSGraphList.ts
@@ -1,5 +1,50 @@
+function walk(
+  graph: WeightedAdjacencyList,
+  curr: number,
+  needle: number,
+  seen: boolean[],
+  path: number[]
+): boolean {
+  if (seen[curr]) {
+    return false;
+  }
+
+  seen[curr] = true;
+
+  // recursion
+  // pre
+  path.push(curr);
+
+  if (curr === needle) {
+    return true;
+  }
+
+  // recurse
+  const list: GraphEdge[] = graph[curr];
+
+  for (let i = 0; i < list.length; i++) {
+    const edge = list[i];
+
+    if (walk(graph, edge.to, needle, seen, path)) {
+      return true;
+    }
+  }
+
+  // post
+  path.pop();
+
+  return false;
+}
+
 export default function dfs(
   graph: WeightedAdjacencyList,
   source: number,
-  needle: number,
-): number[] | null {}
+  needle: number
+): number[] | null {
+  const seen: boolean[] = new Array(graph.length).fill(false);
+  const path: number[] = [];
+
+  walk(graph, source, needle, seen, path);
+
+  return path.length === 0 ? null : path;
+}
